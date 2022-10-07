4ac3209072429cbd7010668d7a87c386ca04b44e
Joyboy <winnerstar29@gmail.com>
2022-10-07 14:04:47 -0400

    colored box done

--- src/bad-components/ColoredBox.tsx
+++ src/bad-components/ColoredBox.tsx
@@ -3,24 +3,25 @@ import { Button } from "react-bootstrap";
 
 export const COLORS = ["red", "blue", "green"];
 const DEFAULT_COLOR_INDEX = 0;
-
-function ChangeColor(): JSX.Element {
-    const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
+interface ChangeColorProps {
+    onchange: () => void;
+}
+function ChangeColor({ onchange }: ChangeColorProps): JSX.Element {
     return (
-        <Button onClick={() => setColorIndex((1 + colorIndex) % COLORS.length)}>
-            Next Color
-        </Button>
+        <>
+            <Button onClick={onchange}>Next Color</Button>
+        </>
     );
 }
 
-function ColorPreview(): JSX.Element {
+function ColorPreview({ color }: { color: number }): JSX.Element {
     return (
         <div
             data-testid="colored-box"
             style={{
                 width: "50px",
                 height: "50px",
-                backgroundColor: COLORS[DEFAULT_COLOR_INDEX],
+                backgroundColor: COLORS[color % COLORS.length], //
                 display: "inline-block",
                 verticalAlign: "bottom",
                 marginLeft: "5px"
@@ -30,13 +31,15 @@ function ColorPreview(): JSX.Element {
 }
 
 export function ColoredBox(): JSX.Element {
+    const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
+    const change = () => setColorIndex(1 + (colorIndex % COLORS.length));
     return (
         <div>
             <h3>Colored Box</h3>
-            <span>The current color is: {COLORS[DEFAULT_COLOR_INDEX]}</span>
+            <span>The current color is: {COLORS[colorIndex]}</span>
             <div>
-                <ChangeColor></ChangeColor>
-                <ColorPreview></ColorPreview>
+                <ChangeColor onchange={change}></ChangeColor>
+                <ColorPreview color={colorIndex}></ColorPreview>
             </div>
         </div>
     );
